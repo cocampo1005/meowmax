38e27e63c461b16d9eaee30124c6cc09fb4c4a91
Christian Ocampo <cocampo1005@yahoo.com>
2025-05-08 18:54:09 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="flex-1 flex items-center justify-center h-screen bg-gradient-to-b from-primary-light-purple to-primary-dark-purple">
+        <h1 className="text-8xl bg-primary-white rounded-2xl p-8 text-primary-dark-purple">
+          Hello World!
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
